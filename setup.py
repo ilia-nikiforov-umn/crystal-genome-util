c5ffd5423117d002fb6d7635cc508e032e99eb6a
ilia Nikiforov <nikif002@umn.edu>
2024-03-08 17:22:46 +0000

    increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="crystal-genome-util",
-    version="0.1.0",
+    version="0.2.0",
     description=(
         "Common functions used by Crystal Genome test drivers"
     ),
