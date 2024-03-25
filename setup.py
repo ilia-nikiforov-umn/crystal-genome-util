e18a2d62fc3e391acbf9c98a28efaebca914b007
ilia Nikiforov <nikif002@umn.edu>
2024-03-25 14:22:45 -0500

    bump kim-property requirement

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     license="LGPL",
-    install_requires=["numpy >= 1.13.1", "ase >= 3.19.0b1", "spglib >= 2.1.0", "kim-edn >= 1.3.1", "kim-property >= 2.5.7"],
+    install_requires=["numpy >= 1.13.1", "ase >= 3.19.0b1", "spglib >= 2.1.0", "kim-edn >= 1.3.1", "kim-property >= 2.6.0"],
     python_requires=">=3.8",
     include_package_data=True
 )
